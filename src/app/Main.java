a5661360ed20f81d9e8eb6c3d7ff0a1d33ab6085
Oleg <olegpanin1968@gmail.com>
2024-06-16 20:56:54 +0300

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,21 @@
+package app;
+
+public class Main {public static void main(String[] args) {
+    Customer customer = getCustomer(getData());
+    String output = "Customer: ".getName() +", phone " + customer.getPhone();
+    getOutput(output);
+}
+    public static String[] getData() {
+        return new String[]{/* пропишіть значення для об'єкту Customer*/};
+    }
+
+    typescript
+    public static Customer getCustomer(String[] data) {
+        return new Customer(data[0], data[1]);
+    }
+
+    public static void getOutput(String output) {
+        System.out.println(output);
+    }
+
+}
